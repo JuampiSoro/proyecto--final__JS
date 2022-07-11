8e7f0fe7e0a82a869309e2335b590c4a3505a0c3
JuampiSoro <jpsoro@Juans-MacBook-Air.local>
2022-07-11 15:24:31 -0300

    Update productos

--- main.js
+++ main.js
@@ -1,94 +1,97 @@
-// // MOSTRAR EN CONSOLA CODIGO DE DESCUENTO
-// console.log("El codigo de descuento es: TUPROMO2022");
-// //
+// MOSTRAR EN CONSOLA CODIGO DE DESCUENTO
+console.log("El codigo de descuento es: TUPROMO2022");
+//
 
 
-// // CREAMOS LA CLASE DE PRODUCTO
-// class Producto {
-//     constructor(nombre, categorias, precio) {
-//         this.nombre = nombre;
-//         this.categorias = categorias;
-//         this.precio = precio;
-//     }
-// }
+// CREAMOS LA CLASE DE PRODUCTO
+class Producto {
+    constructor(nombre, categorias, precio) {
+        this.nombre = nombre;
+        this.categorias = categorias;
+        this.precio = precio;
+    }
+}
 
-// // DECLARAMOS TOTAL DE PRECIO
-// let total = 0;
+// DECLARAMOS TOTAL DE PRECIO
+let total = 0;
 
-// // DECLARAMOS LA LISTA DE PRODUCTOS QUE QUIERE COMPRAR EL USUARIO
-// const listaDeProductosUsuario = [];
+// DECLARAMOS LA LISTA DE PRODUCTOS QUE QUIERE COMPRAR EL USUARIO
+const listaDeProductosUsuario = [];
 
-
-// // DECLARAMOS LOS PRODUCTOS
-// const lecheDeCacahuate = new Producto("Leche de Cacahuate", ["Bebida", "Libre de TACC", "Orgánico"], 270);
-// const castaniasDeCaju = new Producto("Castañas de Cajú", ["Comestible", "Libre de TACC", "Envasadas"], 400);
-// const barraCoco = new Producto("Barra de coco", ["Comestible", "Azúcar", "Coco"], 100);
+// DECLARAMOS LOS ELEMENTOS DEL INDEX
+const btnModeDark = document.querySelector("#nav--btn__mode");
 
 
-// // FUNCIONES
-
-// const funcionEleccionProducto = (productoElegido) => parseInt(prompt(`Ingresaste ${productoElegido}. Ingresá la cantidad que queres de este producto.`));
-
-// const descuento = (total) => total * 0.9;
-
-// const alertTotal = (total) => alert(`Tu total hasta ahora es de ${total}`);
+// DECLARAMOS LOS PRODUCTOS
+const lecheDeCacahuate = new Producto("Leche de Cacahuate", ["Bebida", "Libre de TACC", "Orgánico"], 270);
+const castaniasDeCaju = new Producto("Castañas de Cajú", ["Comestible", "Libre de TACC", "Envasadas"], 400);
+const barraCoco = new Producto("Barra de coco", ["Comestible", "Azúcar", "Coco"], 100);
 
 
+// FUNCIONES
 
-// // COMIENZO DE INTERACCIÓN CON EL USUARIO
-// alert("Bienvenido a Veggie!");
+const funcionEleccionProducto = (productoElegido) => parseInt(prompt(`Ingresaste ${productoElegido}. Ingresá la cantidad que queres de este producto.`));
 
-// let ingresoPrimerProducto = "";
+const descuento = (total) => total * 0.9;
 
-// while (ingresoPrimerProducto !== "salir") {
-//     ingresoPrimerProducto = prompt("Productos disponibles en stock: Leche de Cacahuate - Castañas de Cajú - Barra de Coco. Ingresá tu producto de preferencia. Si no queres agregar mas productos ingresa la palabra SALIR").toLowerCase();
+const alertTotal = (total) => alert(`Tu total hasta ahora es de ${total}`);
 
 
 
-//     if (ingresoPrimerProducto == "leche de cacahuate") {
-//         let cantidad = funcionEleccionProducto(lecheDeCacahuate.nombre);
-//         total += cantidad * lecheDeCacahuate.precio;
-//         alertTotal(total);
-//         listaDeProductosUsuario.push([lecheDeCacahuate.nombre, cantidad]);
+// COMIENZO DE INTERACCIÓN CON EL USUARIO
+alert("Bienvenido a Veggie!");
 
-//     } else if (ingresoPrimerProducto == "castañas de caju" || ingresoPrimerProducto == "castañas de cajú") {
-//         let cantidad = funcionEleccionProducto(castaniasDeCaju.nombre);
-//         total += cantidad * castaniasDeCaju.precio;
-//         alertTotal(total);
-//         listaDeProductosUsuario.push([castaniasDeCaju.nombre, cantidad]);
+let ingresoPrimerProducto = "";
 
-//     } else if (ingresoPrimerProducto == "barra de coco") {
-//         let cantidad = funcionEleccionProducto(barraCoco.nombre);
-//         total += cantidad * barraCoco.precio;
-//         alertTotal(total);
-//         listaDeProductosUsuario.push([barraCoco.nombre, cantidad]);
+while (ingresoPrimerProducto !== "salir") {
+    ingresoPrimerProducto = prompt("Productos disponibles en stock: Leche de Cacahuate - Castañas de Cajú - Barra de Coco. Ingresá tu producto de preferencia. Si no queres agregar mas productos ingresa la palabra SALIR").toLowerCase();
 
-//     } else {
-//         if (total > 0) {
-//             alert("Muchas gracias por confiar en nosotros.")
 
-//             // PREGUNTA SI EL USUARIO TIENE CODIGO DE DESCUENTO
 
-//             const preguntaSiTieneCodigoDescuento = prompt("Tiene codigo de descuento? Responda con un SI o un NO").toLowerCase();
+    if (ingresoPrimerProducto == "leche de cacahuate") {
+        let cantidad = funcionEleccionProducto(lecheDeCacahuate.nombre);
+        total += cantidad * lecheDeCacahuate.precio;
+        alertTotal(total);
+        listaDeProductosUsuario.push([lecheDeCacahuate.nombre, cantidad]);
 
-//             if (preguntaSiTieneCodigoDescuento == "si") {
-//                 const ingresoCodigoDescuento = prompt("Ingresa el codigo de descuento. En la consola lo podes ver tambien").toLowerCase();
+    } else if (ingresoPrimerProducto == "castañas de caju" || ingresoPrimerProducto == "castañas de cajú") {
+        let cantidad = funcionEleccionProducto(castaniasDeCaju.nombre);
+        total += cantidad * castaniasDeCaju.precio;
+        alertTotal(total);
+        listaDeProductosUsuario.push([castaniasDeCaju.nombre, cantidad]);
 
-//                 if (ingresoCodigoDescuento == "tupromo2022") {
-//                     console.log("Ingresaste correctamente el codigo de descuento. ")
-//                     alert(`El total con el descuento del 10% incluido es de ${descuento(total)}`);
-//                 }
-//             }
+    } else if (ingresoPrimerProducto == "barra de coco") {
+        let cantidad = funcionEleccionProducto(barraCoco.nombre);
+        total += cantidad * barraCoco.precio;
+        alertTotal(total);
+        listaDeProductosUsuario.push([barraCoco.nombre, cantidad]);
 
-//             alert(`El total es: $${total}`);
+    } else {
+        if (total > 0) {
+            alert("Muchas gracias por confiar en nosotros.")
 
-//         } else {
-//             alert("No compraste nada :(")
-//         }
-//     }
-// }
+            // PREGUNTA SI EL USUARIO TIENE CODIGO DE DESCUENTO
 
-// console.log(listaDeProductosUsuario);
+            const preguntaSiTieneCodigoDescuento = prompt("Tiene codigo de descuento? Responda con un SI o un NO").toLowerCase();
+
+            if (preguntaSiTieneCodigoDescuento == "si") {
+                const ingresoCodigoDescuento = prompt("Ingresa el codigo de descuento. En la consola lo podes ver tambien").toLowerCase();
+
+                if (ingresoCodigoDescuento == "tupromo2022") {
+                    console.log("Ingresaste correctamente el codigo de descuento. ")
+                    alert(`El total con el descuento del 10% incluido es de ${descuento(total)}`);
+                }
+            }
+
+            alert(`El total es: $${total}`);
+
+        } else {
+            alert("No compraste nada :(")
+        }
+    }
+}
+
+console.log(listaDeProductosUsuario);
 
 
 
