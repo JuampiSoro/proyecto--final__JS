0fd06c70a22bfe50b54d1d82cfa941c5536ab1b4
Juan Pablo Soro <sorojuanpablo@gmail.com>
2022-08-12 09:47:19 -0300

    Release

--- main.js
+++ main.js
@@ -3,52 +3,34 @@ console.log("El codigo de descuento es: TUPROMO2022");
 //
 
 
-// CREAMOS LA CLASE DE PRODUCTO
-// class Producto {
-//     constructor(nombre, categorias, precio) {
-//         this.nombre = nombre;
-//         this.categorias = categorias;
-//         this.precio = precio;
-//     }
-// }
-
 // DECLARAMOS TOTAL DE PRECIO
 let total = 0;
 
-
-
-// ARRAY CON PRODUCTOS
-
-// const productos = [
-//     {
-//         nombre: "Castañas de Cajú",
-//         precio: 400,
-//     },
-//     {
-//         nombre: "Leche de Cacahuate",
-//         precio: 270
-//     },
-//     {
-//         nombre: "Maní tostado salado",
-//         precio: 100,
-//     },
-//     {
-//         nombre: "Maní Japonés Natural",
-//         precio: 200,
-//     }
-
-// ]
-
-
-
+// DECLARAMOS CARRITO 
+let carrito = [];
 
 
 
 // DECLARAMOS LOS ELEMENTOS
 
+// BOTON DE NAVEGACION MOBILE
+
+const nav = document.querySelector("#nav");
+const btnNav = document.querySelector("#nav__ham");
+const ulNav = document.querySelector("#nav--ul");
+
 // ELEMENTO LISTA DE PRODUCTOS - PARA RENDERIZAR PRODUCTOS
 
 const listaProductos = document.querySelector(".main__lista__productos");
+
+
+// DIV CARRITO
+
+const divCarrito = document.querySelector("#main__carrito");
+
+// TEXTO CARRITO VACIO
+const textoCarritoVacio = document.querySelector("#main__carrito__p");
+
 
 
 // ELEMENTOS CÓDIGO DE DESCUENTO
@@ -56,6 +38,14 @@ const inputCodigo = document.querySelector("#main__codigo__input");
 const btnCodigoSend = document.querySelector("#main__codigo__send");
 const seccionIngresarCodigo = document.querySelector("#main__div__ingreso__codigo")
 const cartelCodigoAplicado = document.querySelector("#main__codigo__aplicado");
+
+// BOOL CODIGO DE DESCUENTO PARA APLICAR DESCUENTO SI EL USUARIO INGRESA EL CODIGO DESPUES DE CARGAR EL CARRITO
+
+let boolDescuento = false;
+
+// ELEMENTO PRECIO TOTAL
+const totalPrice = document.querySelector(".main__total__price");
+
 
 // CODIGO DE DESCUENTO EN LOCAL STORAGE
 
@@ -67,17 +57,197 @@ const jsonLocalCodigo = JSON.parse(consultaLocalCodigo);
 // FUNCIONES
 
 // FUNCION PARA RENDER DE CADA PRODUCTO
-const renderEachProducto = (numeroImagen, nombre, precio) => {
+const renderEachProducto = (numeroImagen, nombre, precio, cantidad) => {
 
     const cardProducto = document.createElement("div");
     cardProducto.classList.add("main__producto");
 
     cardProducto.innerHTML = `<img src="imgs/${numeroImagen}.jpeg" alt="${nombre}" class="main__producto__img">
         <h4 class="main__producto__title">${nombre}</h4>
-        <p class="main__producto__precio">${precio}$</p>
-        <button class="main__producto__btn"><span class="main__producto__btn__plus">+</span></button>`
+        <p class="main__producto__precio">${precio}$</p>`
+
+    const btnEachProduct = document.createElement("button");
+    btnEachProduct.classList.add("main__producto__btn");
+    btnEachProduct.innerHTML = `<span class="main__producto__btn__plus">+</span></button>`;
+
+    cardProducto.append(btnEachProduct);
 
     listaProductos.append(cardProducto);
+
+    // const btnAgregarProductoCarrito = document.querySelector(".main__producto__btn");
+
+
+    // CADA BOTON PARA SUMAR AL CARRITO EL PRODUCTO
+
+    btnEachProduct.addEventListener("click", function (e) {
+        const target = e.target.parentElement;
+        // console.log(target);
+
+        if (jsonLocalCodigo == true) {
+            total += precio * 0.9;
+            totalPrice.innerHTML = `${total}`;
+        } else {
+            total += precio;
+            totalPrice.innerHTML = `${total}`;
+        }
+
+        // total += precio;
+
+        const itemAgregado = nombre;
+
+
+        if (carrito.includes(nombre)) {
+            console.log("Juju");
+
+
+        } else {
+            carrito.push(itemAgregado);
+
+            const divEachCarrito = document.createElement("div");
+            divEachCarrito.classList.add("main__carrito__producto")
+
+            divEachCarrito.innerHTML = `<img src="imgs/${numeroImagen}.jpeg" alt="${nombre}" class="main__carrito__producto__img">
+            <div class="main__carrito__producto__container__title">
+                <h3 class="main__carrito__producto__title" id="main__carrito__producto__title">${nombre}</h3>
+                <p>${precio}$</p>
+
+            </div>`
+
+
+            const containerCantidad = document.createElement("div");
+            containerCantidad.classList.add("main__carrito__producto__container__cantidad");
+            containerCantidad.innerHTML = `<p class="main__carrito__producto__cantidad">Cantidad</p>`;
+            divEachCarrito.append(containerCantidad);
+
+            const pCantidad = document.createElement("p");
+            pCantidad.innerText = `${cantidad}`;
+            containerCantidad.append(pCantidad);
+
+            let innerPCantidad = pCantidad.innerText;
+            let addOne = parseInt(innerPCantidad);
+
+
+
+            const btnRemoveItem = document.createElement("button");
+            btnRemoveItem.classList.add("main__producto__btn", "add", "minus");
+            btnRemoveItem.innerHTML = `<span
+            class="main__producto__btn__plus">-</span>`;
+            containerCantidad.append(btnRemoveItem);
+
+
+            const btnAddItem = document.createElement("button");
+            btnAddItem.classList.add("main__producto__btn", "add");
+            btnAddItem.innerHTML = `<span
+            class="main__producto__btn__plus ">+</span>`;
+            containerCantidad.append(btnAddItem);
+
+
+
+
+
+            const containerBtnEliminate = document.createElement("div");
+            containerBtnEliminate.classList.add("order");
+            divEachCarrito.append(containerBtnEliminate);
+
+            const btnEliminate = document.createElement("button");
+            btnEliminate.classList.add("main__carrito__producto__eliminate");
+            btnEliminate.innerHTML = `<ion-icon name="close"></ion-icon>`;
+            containerBtnEliminate.append(btnEliminate);
+
+
+            divCarrito.append(divEachCarrito);
+
+
+            btnEliminate.addEventListener("click", function () {
+                const parent = containerBtnEliminate.parentElement;
+                parent.remove();
+
+                carrito = carrito.filter(e => e !== nombre);
+
+
+
+                // console.log(precio * 0.9);
+
+                // total -= precio;
+                // totalPrice.innerHTML = `${total}`;
+
+                if (jsonLocalCodigo == true || boolDescuento == true) {
+                    total -= (precio * 0.9) * addOne;
+                    totalPrice.innerHTML = `${total}`;
+                } else {
+                    total -= (precio * addOne);
+                    totalPrice.innerHTML = `${total}`;
+                }
+
+
+
+
+            })
+
+            btnAddItem.addEventListener("click", function () {
+
+
+                addOne++;
+
+                pCantidad.innerText = addOne;
+
+
+                if (jsonLocalCodigo == true || boolDescuento == true) {
+                    total += precio * 0.9;
+                    totalPrice.innerHTML = `${total}`;
+                } else {
+                    total += precio;
+                    totalPrice.innerHTML = `${total}`;
+                }
+            })
+
+            btnRemoveItem.addEventListener("click", function () {
+
+                addOne--;
+
+                pCantidad.innerText = addOne;
+
+
+
+
+                if (addOne == 0) {
+                    const parent = containerBtnEliminate.parentElement;
+                    parent.remove();
+                    carrito = carrito.filter(e => e !== nombre);
+                }
+
+
+
+                if (jsonLocalCodigo == true || boolDescuento == true) {
+                    total -= precio * 0.9;
+                    totalPrice.innerHTML = `${total}`;
+                } else {
+                    total -= precio;
+                    totalPrice.innerHTML = `${total}`;
+                }
+            })
+
+
+        }
+
+        console.log(carrito);
+
+
+        // CODIGO DE DESCUENTO
+
+        textoCarritoVacio.style.display = "none";
+
+        if (jsonLocalCodigo == true) {
+            totalPrice.innerHTML = `${total}`;
+
+        } else {
+            // MOSTRAMOS AL USUARIO EL TOTAL
+            totalPrice.innerHTML = `${total}`;
+        }
+
+
+    })
+
 }
 
 
@@ -88,16 +258,12 @@ const pedirProductos = async () => {
     const data = await resp.json();
 
     data.forEach((producto) => {
-        renderEachProducto(producto.numeroImagen, producto.nombre, producto.precio)
+        renderEachProducto(producto.numeroImagen, producto.nombre, producto.precio, producto.cantidad);
     });
 }
 
 
 const funcionEleccionProducto = (productoElegido) => parseInt(prompt(`Ingresaste ${productoElegido}. Ingresá la cantidad que queres de este producto.`));
-
-const descuento = (total) => total * 0.9;
-
-const alertTotal = (total) => alert(`Tu total hasta ahora es de ${total}`);
 
 const notificacionCodigo = (texto, bgColor) => {
     Toastify({
@@ -116,33 +282,26 @@ const notificacionCodigo = (texto, bgColor) => {
 
 pedirProductos();
 
-let ingresoPrimerProducto = "";
 
-while (ingresoPrimerProducto !== "salir") {
-    ingresoPrimerProducto = prompt("Productos disponibles en stock: Leche de Cacahuate - Castañas de Cajú - Barra de Coco. Ingresá tu producto de preferencia. Si no queres agregar mas productos ingresa la palabra SALIR").toLowerCase();
+btnNav.addEventListener("click", () => {
+    nav.style.flexDirection = "column";
+    nav.style.rowGap = "1em";
+    ulNav.style.display = "flex";
+    btnNav.innerHTML = "<ion-icon name='close'></ion-icon>";
 
-
-
-    if (ingresoPrimerProducto == "leche de cacahuate") {
-        let cantidad = funcionEleccionProducto(lecheDeCacahuate.nombre);
-        total += cantidad * lecheDeCacahuate.precio;
-        alertTotal(total);
-
-
-    } else if (ingresoPrimerProducto == "castañas de caju" || ingresoPrimerProducto == "castañas de cajú") {
-        let cantidad = funcionEleccionProducto(castaniasDeCaju.nombre);
-        total += cantidad * castaniasDeCaju.precio;
-        alertTotal(total);
-
-
-    } else if (ingresoPrimerProducto == "barra de coco") {
-        let cantidad = funcionEleccionProducto(barraCoco.nombre);
-        total += cantidad * barraCoco.precio;
-        alertTotal(total);
-
-
+    if (btnNav.classList.contains("delete")) {
+        btnNav.classList.remove("delete");
+        nav.style.flexDirection = "row";
+        ulNav.style.display = "none";
+        btnNav.innerHTML = "<ion-icon name='menu'></ion-icon>";
+    } else {
+        btnNav.classList.add("delete");
     }
-}
+
+})
+
+
+let ingresoPrimerProducto = "";
 
 
 // CODIGO DE DESCUENTO
@@ -159,10 +318,13 @@ btnCodigoSend.addEventListener("click", () => {
     inputValue !== "tupromo2022" && notificacionCodigo("No existe este código.", "red");
 
     if (inputValue == "tupromo2022") {
+        boolDescuento = true;
+        total *= 0.9;
         seccionIngresarCodigo.remove();
         notificacionCodigo("Código aceptado!", "#94B49F");
         cartelCodigoAplicado.style.display = "block";
         localStorage.setItem("CodigoDescuento", true);
+        totalPrice.innerHTML = `${total}`;
     }
 
     inputCodigo.value = "";
